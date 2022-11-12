dff7291512b2935babf79e993ce4d7873a76bc9b
Kaiotech <kaiotech@icloud.com>
2022-11-12 11:35:33 +0100

    initial commit

---
+++ src/main/java/bppueg/BppUegApplication.java
@@ -0,0 +1,13 @@
+package bppueg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BppUegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BppUegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bppueg/BppUegApplicationTests.java
@@ -0,0 +1,13 @@
+package bppueg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BppUegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
